efee5e4edc6b9046b9fcd17783b54ed4d4242813
makhnev-a <ray.makhnev@gmail.com>
2020-08-10 23:37:48 +0300

    Updates

---
+++ precache-manifest.8e4d0b2fd14d50524fc8e30e85976cd0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93fd9600c980bc6d408028415a6adeb",
+    "url": "/virtual-coffee-machine/index.html"
+  },
+  {
+    "revision": "8e58c39a7d685aa65d08",
+    "url": "/virtual-coffee-machine/static/css/main.a8da9462.chunk.css"
+  },
+  {
+    "revision": "2ff02c8fc1774cc84882",
+    "url": "/virtual-coffee-machine/static/js/2.5ad19111.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/virtual-coffee-machine/static/js/2.5ad19111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e58c39a7d685aa65d08",
+    "url": "/virtual-coffee-machine/static/js/main.a4e10650.chunk.js"
+  },
+  {
+    "revision": "f8cd61a251f86179927d",
+    "url": "/virtual-coffee-machine/static/js/runtime-main.7270f675.js"
+  },
+  {
+    "revision": "279dce271ba7ac9512242d6209fd539f",
+    "url": "/virtual-coffee-machine/static/media/coffee.279dce27.jpg"
+  },
+  {
+    "revision": "aa21bd6102bb3e4d3ead112e2ee053bb",
+    "url": "/virtual-coffee-machine/static/media/juice.aa21bd61.jpg"
+  },
+  {
+    "revision": "a590eb30c1779168676c2a4663346ec8",
+    "url": "/virtual-coffee-machine/static/media/milk-coffee.a590eb30.jpeg"
+  },
+  {
+    "revision": "50b22ba175d4645608634c07a1692056",
+    "url": "/virtual-coffee-machine/static/media/tea.50b22ba1.png"
+  }
+]);
